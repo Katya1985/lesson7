cb9533466bf24306a5c71781476af0794b07b354
Katya1985 <berkat85@rambler.ru>
2024-09-09 13:11:36 +0300

    Дополнительное практическое задание по модулю

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 grades = [[5, 3, 3, 5, 4],[2, 2, 2 ,3],[4, 5, 5, 2],[4, 4, 3],[5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-float (grades [0, 1, 2, 3, 4])
-#total = sum (grades [0:len(grades)])
-#print (total)
 list (students)
 res = sorted (students)
 print (res)
-for value in my_list: average= sum (grades [0]) / len (grades [0])
-print (average)
+averge = [sum (i) / len (i) for i in grades]
+print (averge)
+dict_1 = dict (zip(res, averge))
+print (dict_1)
