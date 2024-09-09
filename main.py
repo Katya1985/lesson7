e6c73bbe1c0a7b585b0795ddf7b1e257e5c7c8ad
Katya1985 <berkat85@rambler.ru>
2024-09-09 10:56:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4],[2, 2, 2 ,3],[4, 5, 5, 2],[4, 4, 3],[5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+float (grades [0, 1, 2, 3, 4])
+#total = sum (grades [0:len(grades)])
+#print (total)
+list (students)
+res = sorted (students)
+print (res)
+for value in my_list: average= sum (grades [0]) / len (grades [0])
+print (average)
